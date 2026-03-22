bc5b36c67fd40a7079b308b3b10141475da90fa6
Erik Mikac <5546184+HankSchrader@users.noreply.github.com>
2026-03-21 20:47:19 -0400

    initial commit.

---
+++ src/main/java/com/byte_sized_lessons/build_the_logic/BuildTheLogicApplication.java
@@ -0,0 +1,13 @@
+package com.byte_sized_lessons.build_the_logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildTheLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildTheLogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byte_sized_lessons/build_the_logic/BuildTheLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byte_sized_lessons.build_the_logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildTheLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
